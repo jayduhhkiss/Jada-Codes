c4d508595b2a286925e1cd7f1bc8715508be7b13
Jada - Ashley Hawkins <jah2234@barnard.edu>
2014-10-30 12:06:18 -0400

    Ten Times of Hello World

--- Javascript Basics/myscript.js
+++ Javascript Basics/myscript.js
@@ -1,28 +1,13 @@
-//two console.logs
-console.log("Hello from myscript.js");
-console.log("Hello again!");
+// console.log("Hello World") ... 10 times
+
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
  
-// //define a variable
-// var name = prompt("What is your name?");
-// alert("Hello " + name);
-
-// //reassigned the variable
-// name = "Nick";
-
-// // print in console
-// console.log("The user's name is " + name);
-
-// If Expressions
-console.log("Before");
-
-//false-y values will not print the following. 
-//(0) is false (1) is true
-if(false) {
-	console.log("If block");
-}
-
-else {
-	console.log("Else block");
-}
-
-console.log("After");
