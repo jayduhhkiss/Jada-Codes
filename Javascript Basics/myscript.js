cb137f7e634f0a13c0946b75c7f4c34dad3e3c02
Jada - Ashley Hawkins <jah2234@barnard.edu>
2014-10-30 11:50:31 -0400

    Multiline Comment

--- Javascript Basics/myscript.js
+++ Javascript Basics/myscript.js
@@ -2,12 +2,13 @@
 console.log("Hello from myscript.js");
 console.log("Hello again!");
  
-//added function to variable
+/*
 var name = prompt("What is your name?");
-//alert("Hello " + name);
+alert("Hello " + name);
 
 //reassigned the variable
 name = "Nick";
-
+*/
 // print in console
 console.log("The user's name is " + name);
+
