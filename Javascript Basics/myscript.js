e11c923b1f6ea2b23da72d93edc4bc6b1ca289c8
Jada - Ashley Hawkins <jah2234@barnard.edu>
2014-10-30 11:56:36 -0400

    If Block

--- Javascript Basics/myscript.js
+++ Javascript Basics/myscript.js
@@ -2,13 +2,21 @@
 console.log("Hello from myscript.js");
 console.log("Hello again!");
  
-/*
-var name = prompt("What is your name?");
-alert("Hello " + name);
+// //define a variable
+// var name = prompt("What is your name?");
+// alert("Hello " + name);
 
-//reassigned the variable
-name = "Nick";
-*/
-// print in console
-console.log("The user's name is " + name);
+// //reassigned the variable
+// name = "Nick";
 
+// // print in console
+// console.log("The user's name is " + name);
+
+// If Expressions
+console.log("Before");
+
+if(true) {
+	console.log("If block");
+}
+
+console.log("After");
