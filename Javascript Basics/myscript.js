4f07efc2b5d338be6872597ea60fdffe96ff434a
Jada - Ashley Hawkins <jah2234@barnard.edu>
2014-10-30 11:47:08 -0400

    Comments

--- Javascript Basics/myscript.js
+++ Javascript Basics/myscript.js
@@ -1,10 +1,13 @@
+//two console.logs
 console.log("Hello from myscript.js");
 console.log("Hello again!");
  
+//added function to variable
 var name = prompt("What is your name?");
-
 alert("Hello " + name);
 
+//reassigned the variable
 name = "Nick";
 
+// print in console
 console.log("The user's name is " + name);
