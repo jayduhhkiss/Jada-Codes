7878d93733437839c948b4d86a79a01d0ac41b3a
Jada - Ashley Hawkins <jah2234@barnard.edu>
2014-10-31 19:30:43 -0400

    Looping An Array

--- Javascript Basics/myscript.js
+++ Javascript Basics/myscript.js
@@ -13,3 +13,8 @@ console.log(friends[0]);
 //use var to print
 var friendNumber = 2;
 console.log(friends[friendNumber]);
+
+//loop our friends
+for (var i = 0; i < 4 ; i++) {
+	console.log(friends[i]);
+}
