db042a7c0f910ce5b323c6e543c502f9ef75a018
Jada - Ashley Hawkins <jah2234@barnard.edu>
2014-10-31 19:47:12 -0400

    Object Keys

--- Javascript Basics/myscript.js
+++ Javascript Basics/myscript.js
@@ -3,5 +3,17 @@
 var jada = {
 	first_name: "Jada",
 	middle_name: "Ashley",
-	last_name: "Hawkins"
+	last_name: "Hawkins",
+	"Employee Number": 1
 }
+
+
+//"Object" + "." + "key" accesses key
+console.log(jada.middle_name);
+
+//Must use subscript [] to access string keys
+console.log(jada["Employee Number"]);
+
+var key = "Employee Number";
+
+console.log(jada[key]);
