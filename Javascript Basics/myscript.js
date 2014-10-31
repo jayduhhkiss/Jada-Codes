b8a7d81713ea98d45a13f1179062ca65e7f3f238
Jada - Ashley Hawkins <jah2234@barnard.edu>
2014-10-31 19:37:37 -0400

    Created Jada Object

--- Javascript Basics/myscript.js
+++ Javascript Basics/myscript.js
@@ -1,20 +1,7 @@
-// Array : List of other values
+// Object = A generic object - it has properties
 
-var friends = ["Nick", "Michael", "Amit", "Allison"];
-
-console.log(friends);
-
-// how many friends I have
-console.log(friends.length);
-
-//print Nick
-console.log(friends[0]);
-
-//use var to print
-var friendNumber = 2;
-console.log(friends[friendNumber]);
-
-//loop our friends
-for (var i = 0; i < 4 ; i++) {
-	console.log(friends[i]);
+var jada = {
+	first_name: "Jada",
+	middle_name: "Ashley",
+	last_name: "Hawkins"
 }
