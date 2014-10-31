0938a9e6b7eb8ee2499c2722394c769d13172c33
Jada - Ashley Hawkins <jah2234@barnard.edu>
2014-10-31 19:06:55 -0400

    While Loop - PROMPT
    entering an empty string gives prompt a falsey value

--- Javascript Basics/myscript.js
+++ Javascript Basics/myscript.js
@@ -4,7 +4,12 @@ var counter = 10;
 
 while (counter) {
 	console.log("Hello World");
+	console.log("Hi");
 	counter = counter - 1;
 }
  
+while (prompt("What's your name?")) {
+	console.log("Got your name");
+}
+
 console.log("After");
