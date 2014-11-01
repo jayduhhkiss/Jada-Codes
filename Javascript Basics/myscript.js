5577bcf1c1ee63d4fa9cf8980ffaead22b536030
Jada - Ashley Hawkins <jah2234@barnard.edu>
2014-11-01 12:01:54 -0400

    String Concatenation

--- Javascript Basics/myscript.js
+++ Javascript Basics/myscript.js
@@ -1,57 +1,16 @@
 /*
-* Strings
+* String Concatenation (Addition of Strings)
 */
 
-var name = "Jada";
-console.log(name);
+var part1 = "Hello ";
+var part2 = "World!";
+var whole = part1 + part2 + "!!!"; 
+console.log(whole);
 
-var name2 = 'Jada';
-console.log(name2);
 
-/* 
-* Error! NEED DOUBLE QUOTES!
-* var statement = 'This is Jada's string';
-*/
-var statement = "This is Jada's string";
-console.log(statement);
-
-/* 
-* Error! 
-* var statement2 = 'He said "This is Jim's string"';
-*/
-
-var statement2 = 'He said "This is Jim\'s string"';
-console.log(statement2);
-
-// '\' is an escape character! 
-
-/*
-* Escape Characters
-*/
-
-// Need to use \\ to equal one \ 
-var path = "C:\\folder\\myfile.txt";
-console.log(path);
-
-/* 
-* String with multilines
-* will get error if open & closing quotes 
-* aren't on the same line
-* \n is a new line escape character
-* \t is a tab escape character
-*/
-
-var multiline = "This is line 1\nThis is line 2\n\tThis is line 3";
+var line1 = "This is line1\n";
+var line2 = "This is line2\n";
+var line3 = "\tThis is line3";
+var multiline = line1 + line2 + line3;
 console.log(multiline);
-
-
-
-
-
-
-
-
-
-
-
 
