899fc51dba70e0cca82f55452e075633e9a89125
Jada - Ashley Hawkins <jah2234@barnard.edu>
2014-11-01 13:38:15 -0400

    Notes on Numbers
    Everything is a floating point
    Octal System
    Hexadecimal System

--- Javascript Basics/myscript.js
+++ Javascript Basics/myscript.js
@@ -1,34 +1,55 @@
 /*
-* Comparing Strings
+* Numbers
 */
 
-var first = "Hello";
-var second = "hello";
+// Whole Numbers
+var a = 11 // 11.0
+	b = -123; // -123.0
 
-// Case Matters
-if (first.toLowerCase() === second.toLowerCase()) {
-	console.log("These strings are equal");
-}
+// Floating Point Numbers
+var c = 1.5,
+	d = 123.456789;
 
-else {
-	console.log("The strings are different");
-}
+/*
+* In JavaScript no matter what there is only
+* one type of number. Floating point numbers
+*/
 
-function compare(a, b) {
-	console.log(a + " < " + b, a < b);
-}
+var e = 0.1,
+	f = 0.2; 
 
-// a < b true 
-compare("a", "b");
+/*
+* result
+* 0.020000000000000004
+* rounding errors from floating points operations
+*/
+var result = e * f;
 
-// a < A false 
-compare("a", "A");
+/*
+* Scientific Notiation (1 million)
+*/
+var g = 1000000;
+var gCondensed = 1E6; 
 
-// apples < oranges true 
-compare("apples", "oranges");
 
-// app < apples true 
-compare("app", "apples");
+/*
+* Octal System 
+* Each place represents a power of 8
+* Starting with a 0 is an Octal Base
+*/
+var h = 012;
 
-// hello < hello false 
-compare("hello", "hello");
+/*
+* Hexidecimal System (Base-16)
+* 0 - 9 & A - F
+* 0x...
+*/
+var i = 0xf0;
+
+
+
+
+
+
+
+
