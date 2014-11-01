91ead9155f7f3a6637e4164cb98804cfef38c5ea
Jada - Ashley Hawkins <jah2234@barnard.edu>
2014-11-01 12:44:32 -0400

    Compare Strings

--- Javascript Basics/myscript.js
+++ Javascript Basics/myscript.js
@@ -1,64 +1,34 @@
 /*
-* String Concatenation (Addition of Strings)
+* Comparing Strings
 */
 
-var part1 = "Hello ";
-var part2 = "World!";
-var whole = part1 + part2 + "!!!"; 
-console.log(whole);
+var first = "Hello";
+var second = "hello";
 
-var line1 = "This is line1\n";
-var line2 = "This is line2\n";
-var line3 = "\tThis is line3";
-var multiline = line1 + line2 + line3;
-console.log(multiline);
-
-/*
-* Methods
-*/
-
-var length = whole.length;
-console.log(length);
-
-// Seeing if a string appears in another string
-// The index #6 is the beginning of "World"
-var index = whole.indexOf("World");
-console.log(index);
-
-// indexOf is case sensitive
-// -1 ... can't find!
-var index2 = whole.indexOf("world");
-console.log(index2);
-
-// is W there?
-if (whole.indexOf("W") !== -1) {
-	console.log("W exists in string");
+// Case Matters
+if (first.toLowerCase() === second.toLowerCase()) {
+	console.log("These strings are equal");
 }
 
 else {
-	console.log("W doesn't exist in string");
+	console.log("The strings are different");
 }
 
-// charAt
-console.log(whole.charAt(2));
+function compare(a, b) {
+	console.log(a + " < " + b, a < b);
+}
 
-// finding substring
-// takes in the start index & the length of the string
-var world = whole.substr(6, 5);
-console.log(world);
-console.log(world.toLowerCase());
-console.log(world.toUpperCase());
+// a < b true 
+compare("a", "b");
 
+// a < A false 
+compare("a", "A");
 
+// apples < oranges true 
+compare("apples", "oranges");
 
+// app < apples true 
+compare("app", "apples");
 
-
-
-
-
-
-
-
-
-
-
+// hello < hello false 
+compare("hello", "hello");
