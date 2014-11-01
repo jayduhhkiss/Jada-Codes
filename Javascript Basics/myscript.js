14f4f21f18dcdc7efdbd1b1062e17d44bbd990ca
Jada - Ashley Hawkins <jah2234@barnard.edu>
2014-11-01 16:37:44 -0400

    Adding + Subtracting Array Info

--- Javascript Basics/myscript.js
+++ Javascript Basics/myscript.js
@@ -1,128 +1,82 @@
 /*
-* Numbers
+* Arrays 
 */
 
-// Whole Numbers
-var a = 11 // 11.0
-	b = -123; // -123.0
+var x = ["some", "many", "few", "all"];
+console.log(x.length); // number of elements in array
 
-// Floating Point Numbers
-var c = 1.5,
-	d = 123.456789;
+var y = ['a string', 3, ["a sub array", 3, 4, 5, 6], "treehouse", function(a, b) {return a + b;}];
+console.log(y.length);
+
+var z = [];
+
+// new array
+var a = new Array();
+
+// new array with 50 undefined elements
+var b = new Array(50);
 
 /*
-* In JavaScript no matter what there is only
-* one type of number. Floating point numbers
+* Getting + Setting 
 */
 
-var e = 0.1,
-	f = 0.2; 
+var myArray = ["Hello", 42, true, function(a){return a * 2}];
+
+//get first item in the array
+var word = myArray[0];
+console.log(word);
+
+//get the function
+var doubler = myArray[3];
+console.log(doubler(10));
+
+// set new number
+answer = myArray[1];
+myArray[1] = -123
+var new_answer = myArray[1];
 
 /*
-* result
-* 0.020000000000000004
-* rounding errors from floating points operations
-*/
-var result = e * f;
-
-/*
-* Scientific Notiation (1 million)
-*/
-var g = 1000000;
-var gCondensed = 1E6; 
-
-
-/*
-* Octal System 
-* Each place represents a power of 8
-* Starting with a 0 is an Octal Base
-*/
-var h = 012;
-
-/*
-* Hexidecimal System (Base-16)
-* 0 - 9 & A - F
-* 0x...
-*/
-var i = 0xf0;
-
-/*
-* Parsing Numbers from Strings
-* We've been using numberical literals
-* parseInt()
-* parseFloat() ... gives floating point number
-*/
-var j = parseInt("197");
-var k = "197";
-var o = parseFloat(123.456);
-
-// the second argument allows you to say it is base 10
-var l = parseInt("019", 10);
-var m = parseInt("010111", 2);
-
-/*
-* NaN = Not A Number
-* NaN != NaN
-*/
-var n = parseInt("There are 23 people", 10);
-console.log(isNaN(n));
-
-/*
-* Operations
+* Array Methods
 */
 
-var add = 3 + 3;
-var sub = n - o;
-var mod = 6 % 2; 
-var div = 4 / 5; 
-var mul = 3 * 5;
+var my_array = [2, 3, 4];
 
-/*
-* Comparisons
-*/
+// .toString() represents array as a string
+console.log(my_array.toString());
 
-console.log(1 < 2);
-console.log(1 > 2); 
-console.log(1 <= 2);
-console.log(1 >= 1);
-console.log(1 === 1.0);
-console.log(1 === 1.5);
+// .push('element') adds element to the end of the array
+my_array.push(5);
+console.log(my_array.toString());
 
-// === checks value and type
-console.log(1 === "1"); // false
-console.log(1 == "1"); // true
+// .pop() returns the last value of the array & removes it
+var poppedVar = my_array.pop();
+console.log(poppedVar);
+console.log(my_array.toString());
 
-if (1 < 2) {
-	console.log("Yes");
-}
+// .unshift() adds element to the beginning
+my_array.unshift(4);
+console.log(my_array.toString());
 
-else {
-	console.log("No");
-}
+// .shift() returns first value and removes it
+var shiftedVar = my_array.shift();
+console.log(my_array.toString());
 
-/*
-* The Math Object
-*
-*/ 
 
-// Returns random value between 0 - 1
-var randomNum = Math.random();
-var zeroThruTen = Math.round(Math.random()*10);
 
-// Math.round = rounds normally
-var rounds = Math.round(2.5);
 
-// Math.floor = rounds down
-var floor = Math.floor(2.5);
 
-// Math.ceil = rounds up 
-var ceil = Math.ceil(2.5);
 
-// Math.pow = base , arg 
-var power = Math.pow(2, 2);
 
-// Math.sqrt 
-var sq = Math.sqrt(54);
+
+
+
+
+
+
+
+
+
+
 
 
 
