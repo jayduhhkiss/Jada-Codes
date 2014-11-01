16b2b7a046b903fe3671a2e5009ae4433438c7f3
Jada - Ashley Hawkins <jah2234@barnard.edu>
2014-11-01 11:56:28 -0400

    Escape Characters

--- Javascript Basics/myscript.js
+++ Javascript Basics/myscript.js
@@ -24,3 +24,34 @@ var statement2 = 'He said "This is Jim\'s string"';
 console.log(statement2);
 
 // '\' is an escape character! 
+
+/*
+* Escape Characters
+*/
+
+// Need to use \\ to equal one \ 
+var path = "C:\\folder\\myfile.txt";
+console.log(path);
+
+/* 
+* String with multilines
+* will get error if open & closing quotes 
+* aren't on the same line
+* \n is a new line escape character
+* \t is a tab escape character
+*/
+
+var multiline = "This is line 1\nThis is line 2\n\tThis is line 3";
+console.log(multiline);
+
+
+
+
+
+
+
+
+
+
+
+
